ec8a591bcca4bac79d201aa0a5de27990b691644
siv2r <siv2ram@gmail.com>
2022-05-21 20:22:34 +0530

    examples: add batch_example
    added initial example where the batch context is created and destroyed.

---
+++ examples/batch.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
+#include <secp256k1.h>
+
+int main(void) {
+    /* batch_context uses secp256k1_context only for the error callback function*/
+    secp256k1_context *ctx = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
+    
+    secp256k1_batch_context *batch_ctx = secp256k1_batch_context_create(ctx, 3);
+    assert(batch_ctx != NULL);
+    secp256k1_batch_context_destroy(ctx, batch_ctx);
+
+    secp256k1_context_destroy(ctx);
+
+    printf("Batch example completed...\n");
+    return 0;
+}

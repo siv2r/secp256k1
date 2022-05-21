379cb741fc26cfda1c18f96557bf758ea4ea77c0
siv2r <siv2ram@gmail.com>
2022-05-21 20:14:00 +0530

    batch_interface: add create() and destroy()
    batch_context_create and batch_context_destroy function are compiled
    by default.
    
    src/batch_impl: contains create and destroy function definition.
    
    This implementation is very similar to src/scratch_impl.h

--- src/secp256k1.c
+++ src/secp256k1.c
@@ -32,6 +32,7 @@
 #include "eckey_impl.h"
 #include "hash_impl.h"
 #include "scratch_impl.h"
+#include "batch_impl.h"
 #include "selftest.h"
 
 #ifdef SECP256K1_NO_BUILD
@@ -187,6 +188,17 @@ secp256k1_scratch_space* secp256k1_scratch_space_create(const secp256k1_context*
 void secp256k1_scratch_space_destroy(const secp256k1_context *ctx, secp256k1_scratch_space* scratch) {
     VERIFY_CHECK(ctx != NULL);
     secp256k1_scratch_destroy(&ctx->error_callback, scratch);
+}
+
+secp256k1_batch_context* secp256k1_batch_context_create(const secp256k1_context* ctx, size_t n_terms) {
+    VERIFY_CHECK(ctx != NULL);
+    ARG_CHECK(n_terms != 0);
+    return secp256k1_batch_create(&ctx->error_callback, n_terms);
+}
+
+void secp256k1_batch_context_destroy(const secp256k1_context *ctx, secp256k1_batch_context* batch_ctx) {
+    VERIFY_CHECK(ctx != NULL);
+    secp256k1_batch_destroy(&ctx->error_callback, batch_ctx);
 }
 
 /* Mark memory as no-longer-secret for the purpose of analysing constant-time behaviour

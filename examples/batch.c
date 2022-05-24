0b3d06ec0a6d63a8c9cc9f078213574f6bec7f32
siv2r <siv2ram@gmail.com>
2022-05-24 18:55:08 +0530

    examples: compile only if schnorrsig module is enabled

--- examples/batch.c
+++ examples/batch.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include <secp256k1.h>
+#include <secp256k1_schnorrsig.h>
 
 int main(void) {
     /* batch_context uses secp256k1_context only for the error callback function*/
@@ -10,6 +11,7 @@ int main(void) {
     
     secp256k1_batch_context *batch_ctx = secp256k1_batch_context_create(ctx, 3);
     assert(batch_ctx != NULL);
+    assert(secp256k1_batch_context_verify(ctx, batch_ctx) == 0);
     secp256k1_batch_context_destroy(ctx, batch_ctx);
 
     secp256k1_context_destroy(ctx);

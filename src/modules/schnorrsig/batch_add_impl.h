89cbb2268b5da9c5c63b40d86c3c4d83ef99bcde
siv2r <siv2ram@gmail.com>
2022-05-24 18:50:13 +0530

    batch_interface: add new batch_ctx->rounds member
    batch_ctx->rounds keeps track of number of times the scratch space was
    erased by batch_add_* function. This occurs when user's inputs exceed
    batch object capacity (specified during creation).

--- src/modules/schnorrsig/batch_add_impl.h
+++ src/modules/schnorrsig/batch_add_impl.h
@@ -14,15 +14,22 @@
  *      + R1 + a2*R2 + ... + au*Ru + e1*P1 + (a2*e2)P2 + ... + (au*eu)Pu.
  */
 int secp256k1_batch_verify(const secp256k1_callback* error_callback, secp256k1_batch_context* batch_ctx) {
-    secp256k1_gej resj; 
+    secp256k1_gej resj;
+    int mid_res;
 
-    if(batch_ctx != NULL && batch_ctx->scalars != NULL && batch_ctx->points != NULL) {
-        batch_ctx->result = secp256k1_ecmult_strauss_batch(error_callback, batch_ctx->data, &resj,  batch_ctx->scalars, batch_ctx->points, &batch_ctx->sc_g, NULL, NULL, batch_ctx->len, 0) && secp256k1_gej_is_infinity(&resj);
+    if (batch_ctx->len > 0) {
+        if(batch_ctx->scalars != NULL && batch_ctx->points != NULL) {
+            mid_res = secp256k1_ecmult_strauss_batch(error_callback, batch_ctx->data, &resj,  batch_ctx->scalars, batch_ctx->points, &batch_ctx->sc_g, NULL, NULL, batch_ctx->len, 0) && secp256k1_gej_is_infinity(&resj);
 
-        return batch_ctx->result;
+            if (batch_ctx->rounds > 0){
+                batch_ctx->result = batch_ctx->result && mid_res;
+            } else {
+                batch_ctx->result = mid_res;
+            }
+        }
     }
 
-    return 0;
+    return batch_ctx->result;
 }
 
 #endif

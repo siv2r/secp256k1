b8ca95aad318f39a99c22c6c0f6166ec4af87d67
siv2r <siv2ram@gmail.com>
2022-05-26 20:15:38 +0530

    batch_interface: add partial `batch_add_schnorrsig` func
    This func will be update to include the following:
    [ ] randomizer generation (currently assume to be 1)
    [ ] run transparent verification if batch object is full

--- include/secp256k1_schnorrsig.h
+++ include/secp256k1_schnorrsig.h
@@ -191,6 +191,27 @@ SECP256K1_API int secp256k1_batch_context_verify(
     secp256k1_batch_context *batch_ctx
 ) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2);
 
+/** Adds the given schnorrsig verification data to secp256k1_batch_context
+ *
+ *  Returns 1 on success, 0 on failure.
+ *  Args:    ctx: a secp256k1 context object (can be initialized for none).
+ *     batch_ctx: a secp256k1 batch context object created using 
+ *                the secp256k1_batch_context_create API
+ *  In:    sig64: pointer to the 64-byte signature to verify.
+ *           msg: the message being verified. Can only be NULL if msglen is 0.
+ *        msglen: length of the message
+ *        pubkey: pointer to an x-only public key to verify with (cannot be NULL)
+ */
+SECP256K1_API SECP256K1_WARN_UNUSED_RESULT int secp256k1_batch_context_add_schnorrsig(
+    const secp256k1_context* ctx,
+    secp256k1_batch_context *batch_ctx,
+    const unsigned char *sig64,
+    const unsigned char *msg,
+    size_t msglen,
+    const secp256k1_xonly_pubkey *pubkey
+) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(3) SECP256K1_ARG_NONNULL(6);
+
+
 #ifdef __cplusplus
 }
 #endif

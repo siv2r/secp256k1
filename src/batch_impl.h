6d43d72de7efad36ae996feedc1534a08c8d4902
siv2r <siv2ram@gmail.com>
2022-06-06 21:24:01 +0530

    batch_interface: remove `rounds` data member
    `rounds` was used to track the number of times the scratch space was
    cleared by batch_context_add_* for transparent verification. It will
    also tell the number of terms the user entered (rounds*capacity + len).
    
    Keeping track of this data was not worth it since the user could always
    have a counter to for total number of terms entered. It also made the
    `batch_verify` function look messy. Hence, decided to remove `rounds` memeber.
    
    The side effect of removing this data memeber is setting `batch_ctx->result = 1`
    during creation since we alwasy need to `&&` with `batch_ctx->result` during
    verification.

--- src/batch_impl.h
+++ src/batch_impl.h
@@ -15,13 +15,11 @@
  *       sc_g: scalar corresponding to the generator point in Multi-Scalar
  *             Multiplication equation.
  *        len: number of points (or scalars) presen on batch context's scratch space.
- *     rounds: number of times batch context's scratch space was cleared by batch_add_*,
- *             which happens when the user adds more inputs than the capacity.
- *             than the capacity. 
  *   capacity: max number of inputs (schnorrsigs/tweaks) that the batch object
  *             can hold
  *     result: tells whether the given set of inputs (schnorrsigs/tweaks) is valid
- *             or invalid. 1 = valid and 0 = invalid.
+ *             or invalid. 1 = valid and 0 = invalid. By default, this is set to 1
+ *             during batch context creation (i.e, `secp256k1_batch_create`).
  * 
  *  The following struct name is typdef as secp256k1_batch_context (in include/secp256k1.h). 
  */
@@ -32,7 +30,6 @@ struct secp256k1_batch_context_struct{
     secp256k1_gej *points;
     secp256k1_scalar sc_g;
     size_t len;
-    size_t rounds;
     size_t capacity;
     int result;
 };
@@ -73,9 +70,8 @@ secp256k1_batch_context* secp256k1_batch_create(const secp256k1_callback* error_
         /* set remaining data members */
         secp256k1_scalar_clear(&batch_ctx->sc_g);
         batch_ctx->len = 0;
-        batch_ctx->rounds = 0;
         batch_ctx->capacity = n_terms;
-        batch_ctx->result = 0;
+        batch_ctx->result = 1;
     }
 
     return batch_ctx;
@@ -92,10 +88,10 @@ void secp256k1_batch_destroy(const secp256k1_callback* error_callback, secp256k1
 }
 
 /** Batch verifies the schnorrsig/tweaks present in the batch context object.
- *  If the batch context is empty, 
- * 
- * calls secp256k1_ecmult_strauss_batch on a scratch space filled with 2n points
- * and 2n scalars, where n = no of terms (user input in secp256k1_batch_context_create)
+ *
+ * For computing the multi-scalar point multiplication, calls secp256k1_ecmult_strauss_batch 
+ * on a scratch space filled with 2n points and 2n scalars, where n = no of terms (user input
+ * in secp256k1_batch_context_create)
  * 
  * Fails if:
  * 0 != -(s1 + a2*s2 + ... + au*su)G
@@ -108,11 +104,7 @@ int secp256k1_batch_verify(const secp256k1_callback* error_callback, secp256k1_b
     if (batch_ctx->len > 0) {
         if(batch_ctx->scalars != NULL && batch_ctx->points != NULL) {
             mid_res = secp256k1_ecmult_strauss_batch(error_callback, batch_ctx->data, &resj, batch_ctx->scalars, batch_ctx->points, &batch_ctx->sc_g, NULL, NULL, batch_ctx->len, 0) && secp256k1_gej_is_infinity(&resj);
-            if (batch_ctx->rounds > 0){
-                batch_ctx->result = batch_ctx->result && mid_res;
-            } else {
-                batch_ctx->result = mid_res;
-            }
+            batch_ctx->result = batch_ctx->result && mid_res;
         }
     }
 

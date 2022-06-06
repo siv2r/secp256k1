03fcaa594c876a5bfcd38d444e2d4fc9e4c20ba3
siv2r <siv2ram@gmail.com>
2022-06-06 22:18:34 +0530

    examples: improve documentation

--- examples/batch.c
+++ examples/batch.c
@@ -15,36 +15,82 @@ unsigned char sig[N_SIGS][64];
 secp256k1_keypair keypair;
 secp256k1_xonly_pubkey pk;
 
-void generate_inp_data(secp256k1_context *ctx) {
-    size_t i;
-    /* key pair generation */
-    assert(fill_random(sk, sizeof(sk)));
-    assert(secp256k1_keypair_create(ctx, &keypair, sk));
-    assert(secp256k1_keypair_xonly_pub(ctx, &pk, NULL, &keypair));
+/* generate key pair required for sign and verify */
+int create_keypair(secp256k1_context *ctx) {
+    while(1) {
+        if (!fill_random(sk, sizeof(sk))) {
+            printf("Failed to generate randomness\n");
+            return 1;
+        }
+        if (secp256k1_keypair_create(ctx, &keypair, sk)) {
+            break;
+        }
+    }
+    if (!secp256k1_keypair_xonly_pub(ctx, &pk, NULL, &keypair)) {
+        return 0;
+    }
 
-    /* create schnorrsig for N_SIGS random messages */
+    return 1;
+}
+
+/* create valid schnorrsigs for N_SIGS random messages */
+int generate_schnorrsigs(secp256k1_context *ctx) {
+    size_t i;
+
     for (i = 0; i < N_SIGS; i++) {
-        assert(fill_random(msg[i], sizeof(msg[i])));
+        if(!fill_random(msg[i], sizeof(msg[i]))) {
+            printf("Failed to generate randomness\n");
+            return 1;            
+        }
         assert(secp256k1_schnorrsig_sign32(ctx, sig[i], msg[i], &keypair, NULL));
         assert(secp256k1_schnorrsig_verify(ctx, sig[i], msg[i], sizeof(msg[i]), &pk));
     }
+
+    return 1;
 }
 
 int main(void) {
     int ret;
+    size_t i;
     /* batch_context uses secp256k1_context only for the error callback function*/
     secp256k1_context *ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
     secp256k1_batch_context *batch_ctx = secp256k1_batch_context_create(ctx, N_SIGS);
 
-    generate_inp_data(ctx);
+    /* key pair generation */
+    printf("Creating a key pair.......................");
+    if(!create_keypair(ctx)) {
+        printf("FAILED\n");
+        return 1;
+    }
+    printf("ok\n");
 
-    ret = secp256k1_batch_context_add_schnorrsig(ctx, batch_ctx, sig[0], msg[0], sizeof(msg[0]), &pk);
-    assert(ret);
-    assert(secp256k1_batch_context_verify(ctx, batch_ctx));
+    /* create schnorrsigs for N_SIGS random messages */
+    printf("Signing messages..........................");
+    if(!generate_schnorrsigs(ctx)) {
+        printf("FAILED\n");
+        return 1;
+    }
+    printf("ok\n");
+
+    printf("Adding signatures to the batch context....");
+    for (i = 0; i < N_SIGS; i++) {
+        ret = secp256k1_batch_context_add_schnorrsig(ctx, batch_ctx, sig[i], msg[i], sizeof(msg[i]), &pk);
+        if(!ret) {
+            printf("FAILED\n");
+            return 1;
+        }
+    }
+    printf("ok\n");
+
+    printf("Verifying the batch context...............");
+    if(!secp256k1_batch_context_verify(ctx, batch_ctx)) {
+        printf("FAILED\n");
+        return 1;
+    }
+    printf("ok\n");
 
     secp256k1_batch_context_destroy(ctx, batch_ctx);
     secp256k1_context_destroy(ctx);
 
-    printf("Batch example completed...\n");
     return 0;
 }

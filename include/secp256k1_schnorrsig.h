cff18eba3b57915fd8765b8ae91aef932174fd97
siv2r <siv2ram@gmail.com>
2022-06-08 06:05:01 +0530

    batch_interface: impl `batch_context_add_xonlypub_tweak`
    Adds xonly pubkey tweak check data to the batch context.
    This function's algorithm is based on `secp256k1_xonly_pubkey_tweak_add_check`.

--- include/secp256k1_schnorrsig.h
+++ include/secp256k1_schnorrsig.h
@@ -175,7 +175,7 @@ SECP256K1_API SECP256K1_WARN_UNUSED_RESULT int secp256k1_schnorrsig_verify(
     const secp256k1_xonly_pubkey *pubkey
 ) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(5);
 
-/** Adds the given schnorrsig verification data to secp256k1_batch_context
+/** Adds the given schnorrsig verification data to secp256k1_batch_context.
  *
  *  Returns 1 on success, 0 on failure.
  *  Args:    ctx: a secp256k1 context object (can be initialized for none).
@@ -194,6 +194,34 @@ SECP256K1_API SECP256K1_WARN_UNUSED_RESULT int secp256k1_batch_context_add_schno
     size_t msglen,
     const secp256k1_xonly_pubkey *pubkey
 ) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(3) SECP256K1_ARG_NONNULL(6);
+
+/** Adds the given tweaked pubkey, internal pubkey, and tweak32 to secp256k1_batch_context.
+ *
+ *  The tweaked pubkey is represented by its 32-byte x-only serialization and
+ *  its pk_parity, which can both be obtained by converting the result of
+ *  tweak_add to a secp256k1_xonly_pubkey.
+ *
+ *  Returns 1 on success, 0 on failure.
+ *  Args:            ctx: pointer to a context object initialized for verification.
+ *             batch_ctx: a secp256k1 batch context object created using
+ *                        the secp256k1_batch_context_create API.
+ *  In: tweaked_pubkey32: pointer to a serialized xonly_pubkey.
+ *     tweaked_pk_parity: the parity of the tweaked pubkey (whose serialization
+ *                        is passed in as tweaked_pubkey32). This must match the
+ *                        pk_parity value that is returned when calling
+ *                        secp256k1_xonly_pubkey with the tweaked pubkey, or
+ *                        this function will fail.
+ *       internal_pubkey: pointer to an x-only public key object to apply the tweak to.
+ *               tweak32: pointer to a 32-byte tweak.
+ */
+SECP256K1_API SECP256K1_WARN_UNUSED_RESULT int secp256k1_batch_context_add_xonlypub_tweak(
+    const secp256k1_context* ctx,
+    secp256k1_batch_context *batch_ctx,
+    const unsigned char *tweaked_pubkey32,
+    int tweaked_pk_parity,
+    const secp256k1_xonly_pubkey *internal_pubkey,
+    const unsigned char *tweak32
+) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3) SECP256K1_ARG_NONNULL(5) SECP256K1_ARG_NONNULL(6);
 
 
 #ifdef __cplusplus

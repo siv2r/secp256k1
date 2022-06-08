862e4f2b541caeb972da464858c54f596184e52b
siv2r <siv2ram@gmail.com>
2022-06-08 06:05:01 +0530

    batch_interface: impl `batch_context_add_xonlypub_tweak`
    Adds xonly pubkey tweak check data to the batch context.
    This function's algorithm is based on `secp256k1_xonly_pubkey_tweak_add_check`.

--- src/modules/schnorrsig/batch_add_impl.h
+++ src/modules/schnorrsig/batch_add_impl.h
@@ -4,13 +4,14 @@
 #include "../../batch_impl.h"
 
 /** Adds the given schnorrsig data to the batch context.
+ *  This function's algorithm is based on secp256k1_schnorrsig_verify.
  *
  *  appends (ai, R), (ai.e, P) to the batch context's scratch space
  *  R = nonce commitment - secp256k1_gej
- *  X = pubkey - secp256k1_gej
+ *  P = pubkey - secp256k1_gej
  *  ai = randomizer - secp256k1_scalar
  *
- *  increments the scalar of G (in the batch context) by -ai.s
+ *  increments the scalar of G (in the batch context) by -ai.s, where
  *  s = sig64[32:64]
  */
 /*todo: run transparent verification, if batch is full */
@@ -89,6 +90,92 @@ int secp256k1_batch_context_add_schnorrsig(const secp256k1_context* ctx, secp256
     secp256k1_scalar_mul(&s, &s, &ai);
     secp256k1_scalar_negate(&s, &s);
     secp256k1_scalar_add(&batch_ctx->sc_g, &batch_ctx->sc_g, &s);
+
+    batch_ctx->len += 2;
+
+    return 1;
+}
+
+/** Adds the given xonly pubkey tweak check data to the batch context.
+ *  This function's algorithm is based on secp256k1_xonly_pubkey_tweak_add_check.
+ *
+ *  appends (-ai, Q), (ai, P) to the batch context's scratch space
+ *  Q = tweaked pubkey - secp256k1_gej
+ *  P = internal pubkey - secp256k1_gej
+ *  ai = randomizer - secp256k1_scalar
+ *
+ *  increments the scalar of G (in the batch context) by ai.tweak times
+ */
+/*todo: run transparent verification, if batch is full */
+/* todo: any MAX/2 check required? (like in prev impl) */
+int secp256k1_batch_context_add_xonlypub_tweak(const secp256k1_context* ctx, secp256k1_batch_context *batch_ctx, const unsigned char *tweaked_pubkey32, int tweaked_pk_parity, const secp256k1_xonly_pubkey *internal_pubkey,const unsigned char *tweak32) {
+    secp256k1_scalar tweak;
+    secp256k1_scalar ai;
+    secp256k1_scalar tmp;
+    secp256k1_ge pk;
+    secp256k1_ge q;
+    secp256k1_fe qx;
+    int overflow;
+    size_t i;
+
+    VERIFY_CHECK(ctx != NULL);
+    ARG_CHECK(internal_pubkey != NULL);
+    ARG_CHECK(tweaked_pubkey32 != NULL);
+    ARG_CHECK(tweak32 != NULL);
+
+    if (!secp256k1_fe_set_b32(&qx, tweaked_pubkey32)) {
+        return 0;
+    }
+
+    secp256k1_scalar_set_b32(&tweak, tweak32, &overflow);
+    if (overflow) {
+        return 0;
+    }
+
+    if (!secp256k1_xonly_pubkey_load(ctx, &pk, internal_pubkey)) {
+        return 0;
+    }
+
+    /* run verify if batch context's scratch is full */
+    /* todo: create a function to do this?? */
+    if (batch_ctx->capacity - batch_ctx->len < 2) {
+        printf("\nbatch_add_xonlypub_tweak: Batch context is full...\n");
+        printf("batch_add_xonlypub_tweak: Verifying the batch context...\n");
+        if (!secp256k1_batch_verify(&ctx->error_callback, batch_ctx)) {
+            /* tell user there is no point in adding sigs/tweaks?? */
+            /* it will fail anyway */
+        }
+        printf("batch_add_xonlypub_tweak: Clearing the batch context for future use...\n");
+        secp256k1_batch_scratch_clear(&ctx->error_callback, batch_ctx);
+        if (!secp256k1_batch_scratch_alloc(&ctx->error_callback, batch_ctx)) {
+            return 0;
+        }
+    }
+
+    i = batch_ctx->len;
+    /* append point Q to the scratch space */
+    if (!secp256k1_ge_set_xo_var(&q, &qx, tweaked_pk_parity)) {
+        return 0;
+    }
+    if (!secp256k1_ge_is_in_correct_subgroup(&q)) {
+        return 0;
+    }
+    secp256k1_gej_set_ge(&batch_ctx->points[i], &q);
+
+    /* append point P to the scratch space */
+    secp256k1_gej_set_ge(&batch_ctx->points[i+1], &pk);
+
+    /* Compute ai */
+    secp256k1_scalar_set_int(&ai, 1);
+
+    /* append scalars -ai, ai respectively to scratch space */
+    secp256k1_scalar_negate(&tmp, &ai);
+    batch_ctx->scalars[i] = tmp;
+    batch_ctx->scalars[i+1] = ai;
+
+    /* increment scalar of G by ai.tweak */
+    secp256k1_scalar_mul(&tweak, &tweak, &ai);
+    secp256k1_scalar_add(&batch_ctx->sc_g, &batch_ctx->sc_g, &tweak);
 
     batch_ctx->len += 2;
 

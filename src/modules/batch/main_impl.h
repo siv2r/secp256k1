40766cf4a3611ec7854291da6b00d0c72e2a3603
siv2r <siv2ram@gmail.com>
2022-06-20 11:12:16 +0530

    batch_interface: initialize empty batch module (experimental)

---
+++ src/modules/batch/main_impl.h
@@ -0,0 +1,12 @@
+/**********************************************************************
+ * Copyright (c) 2022 Jonas Nick, Sivaram Dhakshinamoorthy            *
+ * Distributed under the MIT software license, see the accompanying   *
+ * file COPYING or http://www.opensource.org/licenses/mit-license.php.*
+ **********************************************************************/
+
+#ifndef SECP256K1_MODULE_BATCH_MAIN_H
+#define SECP256K1_MODULE_BATCH_MAIN_H
+
+#include "include/secp256k1_batch.h"
+
+#endif /* SECP256K1_MODULE_BATCH_MAIN_H */

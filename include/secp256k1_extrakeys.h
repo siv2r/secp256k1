7297799528564cf59002e17652c83304733e5f07
siv2r <siv2ram@gmail.com>
2022-06-21 06:24:59 +0530

    batch_add_tweaks: move to extrakeys module

--- include/secp256k1_extrakeys.h
+++ include/secp256k1_extrakeys.h
@@ -2,6 +2,7 @@
 #define SECP256K1_EXTRAKEYS_H
 
 #include "secp256k1.h"
+#include "include/secp256k1_batch.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -241,6 +242,33 @@ SECP256K1_API SECP256K1_WARN_UNUSED_RESULT int secp256k1_keypair_xonly_tweak_add
     secp256k1_keypair *keypair,
     const unsigned char *tweak32
 ) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3);
+
+/** Adds the given tweaked pubkey check data to secp256k1_batch.
+ *
+ *  The tweaked pubkey is represented by its 32-byte x-only serialization and
+ *  its pk_parity, which can both be obtained by converting the result of
+ *  tweak_add to a secp256k1_xonly_pubkey.
+ *
+ *  Returns 1 on success, 0 on failure.
+ *  Args:            ctx: pointer to a context object initialized for verification.
+ *                 batch: a secp256k1 batch object created using `secp256k1_batch_create`.
+ *  In: tweaked_pubkey32: pointer to a serialized xonly_pubkey.
+ *     tweaked_pk_parity: the parity of the tweaked pubkey (whose serialization
+ *                        is passed in as tweaked_pubkey32). This must match the
+ *                        pk_parity value that is returned when calling
+ *                        secp256k1_xonly_pubkey with the tweaked pubkey, or
+ *                        this function will fail.
+ *       internal_pubkey: pointer to an x-only public key object to apply the tweak to.
+ *               tweak32: pointer to a 32-byte tweak.
+ */
+SECP256K1_API SECP256K1_WARN_UNUSED_RESULT int secp256k1_batch_add_xonlypub_tweak_check(
+    const secp256k1_context* ctx,
+    secp256k1_batch *batch,
+    const unsigned char *tweaked_pubkey32,
+    int tweaked_pk_parity,
+    const secp256k1_xonly_pubkey *internal_pubkey,
+    const unsigned char *tweak32
+) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3) SECP256K1_ARG_NONNULL(5) SECP256K1_ARG_NONNULL(6);
 
 #ifdef __cplusplus
 }

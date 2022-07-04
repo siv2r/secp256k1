813056928dc8da8475110bf5c9cd6a41c2e9417e
siv2r <siv2ram@gmail.com>
2022-07-04 17:29:06 +0530

    tests: add batch_add_schnorrsig_tests to test_batch_api()

--- src/modules/batch/tests_impl.h
+++ src/modules/batch/tests_impl.h
@@ -3,7 +3,9 @@
 
 #include "include/secp256k1_batch.h"
 
-#define MAX_TERMS 10
+#define MAX_TERMS 20
+#define N_SIGS 10
+#define N_TWK_CHECKS 10
 
 /* Tests for the equality of two sha256 structs. This function only produces a
  * correct result if an integer multiple of 64 many bytes have been written
@@ -29,7 +31,20 @@ void test_batch_sha256_tagged(void) {
 }
 
 void test_batch_api(void) {
-    /** setup **/
+    unsigned char sk[32];
+    secp256k1_keypair keypair;
+    secp256k1_xonly_pubkey pk;
+    secp256k1_xonly_pubkey zero_pk;
+    /* schnorr verification data */
+    unsigned char msg[N_SIGS][32];
+    unsigned char sig[N_SIGS][64];
+    /* xonly pubkey tweak checks data */
+    unsigned char tweaked_pk[N_TWK_CHECKS][32];
+    int tweaked_pk_parity[N_TWK_CHECKS];
+    unsigned char tweak[N_TWK_CHECKS][32];
+    secp256k1_pubkey tmp_pk;
+    secp256k1_xonly_pubkey tmp_xonly_pk;
+    /* context and batch setup */
     secp256k1_context *none = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
     secp256k1_context *sign = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
     secp256k1_context *vrfy = secp256k1_context_create(SECP256K1_CONTEXT_VERIFY);
@@ -40,7 +55,9 @@ void test_batch_api(void) {
     secp256k1_batch *batch_vrfy;
     secp256k1_batch *batch_both;
     secp256k1_batch *batch_sttc;
+    int batch_reset; /* checks batch limit exceed */
     int ecount;
+    size_t i;
 
     secp256k1_context_set_error_callback(none, counting_illegal_callback_fn, &ecount);
     secp256k1_context_set_error_callback(sign, counting_illegal_callback_fn, &ecount);
@@ -53,10 +70,33 @@ void test_batch_api(void) {
     secp256k1_context_set_illegal_callback(both, counting_illegal_callback_fn, &ecount);
     secp256k1_context_set_illegal_callback(sttc, counting_illegal_callback_fn, &ecount);
 
+    /* generate keypair data */
+    secp256k1_testrand256(sk);
+    CHECK(secp256k1_keypair_create(ctx, &keypair, sk) == 1);
+    CHECK(secp256k1_keypair_xonly_pub(ctx, &pk, NULL, &keypair) == 1);
+    memset(&zero_pk, 0, sizeof(zero_pk));
+
+    /* generate N_SIGS schnorr verify data (msg[32], sig[64]) */
+    for (i = 0; i < N_SIGS; i++) {
+        secp256k1_testrand256(msg[i]);
+        CHECK(secp256k1_schnorrsig_sign32(ctx, sig[i], msg[i], &keypair, NULL) == 1);
+        CHECK(secp256k1_schnorrsig_verify(ctx, sig[i], msg[i], sizeof(msg[i]), &pk));
+    }
+
+    /* generate N_TWK_CHECKS tweak check data (tweaked_pk[32], tweaked_pk_parity, tweak[32]) */
+    for (i = 0; i < N_TWK_CHECKS; i++) {
+        secp256k1_testrand256(tweak[i]);
+        CHECK(secp256k1_xonly_pubkey_tweak_add(ctx, &tmp_pk, &pk, tweak[i]));
+        CHECK(secp256k1_xonly_pubkey_from_pubkey(ctx, &tmp_xonly_pk, &tweaked_pk_parity[i], &tmp_pk));
+        CHECK(secp256k1_xonly_pubkey_serialize(ctx, tweaked_pk[i], &tmp_xonly_pk));
+        CHECK(secp256k1_xonly_pubkey_tweak_add_check(ctx, tweaked_pk[i], tweaked_pk_parity[i], &pk, tweak[i]));
+    }
+
+
     /** main test body **/
     /* todo: need to add tests for 1/4th, 3/4th of MAX_TERMS size? */
     ecount = 0;
-    batch_none = secp256k1_batch_create(none, MAX_TERMS);
+    batch_none = secp256k1_batch_create(none, 1);
     CHECK(batch_none != NULL);
     CHECK(ecount == 0);
     batch_sign = secp256k1_batch_create(sign, MAX_TERMS/2);
@@ -65,7 +105,7 @@ void test_batch_api(void) {
     batch_vrfy = secp256k1_batch_create(vrfy, MAX_TERMS-1);
     CHECK(batch_vrfy != NULL);
     CHECK(ecount == 0);
-    batch_both = secp256k1_batch_create(both, 1);
+    batch_both = secp256k1_batch_create(both, MAX_TERMS);
     CHECK(batch_both != NULL);
     CHECK(ecount == 0);
     /* ARG_CHECK(max_terms != 0) in `batch_create` should fail*/
@@ -77,8 +117,45 @@ void test_batch_api(void) {
     CHECK(batch_sttc == NULL);
     CHECK(ecount == 2);
 
+    /* secp256k1_batch_add tests for batch_none */
     ecount = 0;
-    CHECK(secp256k1_batch_verify(none, batch_none) == 1);
+    /* CHECK(secp256k1_batch_add_schnorrsig(none, batch_none, sig[0], msg[0], sizeof(msg[0]), &pk, &batch_reset) == 1);
+    CHECK(batch_reset == 0);
+    CHECK(ecount == 0);
+    CHECK(secp256k1_batch_add_schnorrsig(none, batch_none, sig[0], msg[0], sizeof(msg[0]), &pk, &batch_reset) == 1);
+    CHECK(batch_reset == 1);
+    CHECK(ecount == 0); */
+    CHECK(secp256k1_batch_add_schnorrsig(none, batch_none, NULL, msg[0], sizeof(msg[0]), &pk, &batch_reset) == 0);
+    CHECK(ecount == 1);
+    CHECK(secp256k1_batch_add_schnorrsig(none, batch_none, sig[0], NULL, sizeof(msg[0]), &pk, &batch_reset) == 0);
+    CHECK(ecount == 2);
+    /* todo: should batch add return 1, here? --> this fails on verify */
+    CHECK(secp256k1_batch_add_schnorrsig(none, batch_none, sig[0], NULL, 0, &pk, &batch_reset) == 1);
+    CHECK(ecount == 2); /*todo: shouldn't this fail, since ARG_CHECK(msg != NULL || msglen  == 0)?*/
+    CHECK(secp256k1_batch_add_schnorrsig(none, batch_none, sig[0], msg[0], sizeof(msg[0]), NULL, &batch_reset) == 0);
+    CHECK(ecount == 3);
+    CHECK(secp256k1_batch_add_schnorrsig(none, batch_none, sig[0], msg[0], sizeof(msg[0]), &zero_pk, &batch_reset) == 0);
+    CHECK(ecount == 4);
+
+    /* secp256k1_batch_add_tests for batch_sign */
+    ecount = 0;
+
+    /* secp256k1_batch_add_tests for batch_vrfy */
+    ecount = 0;
+
+    /* secp256k1_batch_add_tests for batch_both */
+    ecount = 0;
+    for (i = 0; i < N_SIGS; i++) {
+        CHECK(secp256k1_batch_isvalid(ctx, batch_both) == 1);
+        CHECK(secp256k1_batch_add_schnorrsig(ctx, batch_both, sig[i], msg[i], sizeof(msg[i]), &pk, &batch_reset) == 1);
+    }
+    for (i = 0; i < N_TWK_CHECKS; i++) {
+        CHECK(secp256k1_batch_isvalid(ctx, batch_both));
+        CHECK(secp256k1_batch_add_xonlypub_tweak_check(ctx, batch_both, tweaked_pk[i], tweaked_pk_parity[i], &pk, tweak[i], &batch_reset));
+    }
+
+    ecount = 0;
+    CHECK(secp256k1_batch_verify(none, batch_none) == 0);
     CHECK(ecount == 0);
     CHECK(secp256k1_batch_verify(sign, batch_sign) == 1);
     CHECK(ecount == 0);

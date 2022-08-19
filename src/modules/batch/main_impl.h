b868ccc4ad6778dccbd5230d661a3097cd6b8e1a
siv2r <siv2ram@gmail.com>
2022-08-19 13:53:00 +0530

    batch: Generate graphs for batch verify speed up This commit generates graphs that visualize the batch verify speed up over single verification (y-axis) wrt the number of signatures (or tweak checks) in the batch. The input data points are taken from the batch verify benchmark.
    GNU plot was used to generate these graphs. The instructions to reproduce these
    graphs (on your local machine) are given in doc/speedup-batch.md
    
    The value of `STRAUSS_MAX_TERMS_PER_BATCH` was calculated (approx) from
    the generated graphs. Relevant discussion: https://github.com/siv2r/secp256k1/pull/2#issuecomment-1211585236

--- src/modules/batch/main_impl.h
+++ src/modules/batch/main_impl.h
@@ -2,6 +2,12 @@
 #define SECP256K1_MODULE_BATCH_MAIN_H
 
 #include "include/secp256k1_batch.h"
+#include "src/hash.h"
+#include "src/scratch.h"
+
+/* Maximum number of scalar-point pairs on the batch
+ * for which `secp256k1_batch_verify` remains efficient */
+#define STRAUSS_MAX_TERMS_PER_BATCH 106
 
 /* Assume two batch objects (batch1 and batch2) and we call
  * `batch_add_tweak_check` on batch1 and `batch_add_schnorrsig` on batch2.
@@ -102,6 +108,10 @@ secp256k1_batch* secp256k1_batch_create(const secp256k1_context* ctx, size_t max
     secp256k1_batch* batch;
     size_t batch_scratch_size;
     unsigned char zeros[16] = {0};
+    /* max number of scalar-point pairs on scratch up to which Strauss multi multiplication is efficient */
+    if (max_terms > STRAUSS_MAX_TERMS_PER_BATCH) {
+        max_terms = STRAUSS_MAX_TERMS_PER_BATCH;
+    }
 
     VERIFY_CHECK(ctx != NULL);
     ARG_CHECK(max_terms != 0);

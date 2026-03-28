2fe4b5ebf22699319e5cc300ecfc2ed69e63ec3b
siv2r <siv2ram@gmail.com>
2026-03-28 16:11:08 +0530

    generate graphs with 16MB mem_limit

--- src/modules/extrakeys/bench_impl.h
+++ src/modules/extrakeys/bench_impl.h
@@ -63,7 +63,7 @@ static void run_extrakeys_bench(int iters, int argc, char** argv) {
     data.tweaked_pk_parities = (const int **)malloc(iters * sizeof(int *));
     data.tweaks = (const unsigned char **)malloc(iters * sizeof(unsigned char *));
 #ifdef ENABLE_MODULE_BATCH
-    data.batch = secp256k1_batch_create(data.ctx, 4 * 1024 * 1024, NULL);
+    data.batch = secp256k1_batch_create(data.ctx, 16 * 1024 * 1024, NULL);
     CHECK(data.batch != NULL);
 #endif
 
